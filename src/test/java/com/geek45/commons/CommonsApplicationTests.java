6697b460db873c556684b28172488cfb401c812a
rubik <rubixgeek795@gmail.com>
2021-03-29 21:07:29 +0800

    添加HTTPClient工具类

--- src/test/java/com/geek45/commons/CommonsApplicationTests.java
+++ src/test/java/com/geek45/commons/CommonsApplicationTests.java
@@ -2,23 +2,31 @@ package com.geek45.commons;
 
 import com.geek45.commons.bean.BeanUtil;
 import com.geek45.commons.dto.User;
+import com.geek45.commons.http.HttpClientUtil;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Map;
 
 @SpringBootTest
 class CommonsApplicationTests {
+	private static final Logger log = LoggerFactory.getLogger(CommonsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void copyTest(){
-		User src = new User();
-		src.setGender("1");
-		src.setName("tes");
-		User des = null;
+	void testHttpClient() throws IOException {
+		String url = "https://www.baidu.com";
 
+		Map<String, Object> result = HttpClientUtil.doGet(url);
+		String resultStr = result.get("result").toString();
+		log.info(resultStr);
 	}
+
 
 }
